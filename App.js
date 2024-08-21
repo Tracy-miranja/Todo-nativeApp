1ad93128c984270aaacdf98f6ea3e9edacd2e085
Tracy-miranja <barazatracy44@gmail.com>
2024-08-21 07:19:16 +0300

    Add data

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TextInput, View } from "react-native";
-import TodoScreen from "./src/screen/TodoScreen";
-import { SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  SafeAreaView,
+  FlatList,
+} from "react-native";
+
+const dummyData = [
+  {
+    id: "1",
+    title: "Practice react native",
+    Duration: "3hrs",
+  },
+  {
+    id: "2",
+    title: "Go to the gym",
+    Duration: "1hr",
+  },
+  {
+    id: "3",
+    title: "Take a cold shower",
+    Duration: "30min",
+  },
+];
 
 export default function App() {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ flex: 1 }}>
       <View style={{ marginHorizontal: 16 }}>
-        <TodoScreen />
         <TextInput
           style={{
             borderWidth: 2,
-            borderColor: "#00ff87",
-            marginTop: 10,
-            borderRadius: 20,
+            borderColor: "#000",
+            marginTop: 60,
+            borderRadius: 5,
+            paddingVertical: 1,
+            paddingHorizontal: 16,
           }}
+          placeholder="Add a task"
         />
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#00ff87",
+            borderRadius: 20,
+            padding: 2,
+            marginTop: 5,
+            alignItems: "center", // alignText should be alignItems
+          }}
+        >
+          <Text style={{ color: "#fff", fontWeight: "bold" }}>Add</Text>
+        </TouchableOpacity>
+        {/* render Todo list */}
+        {/* <FlatList data={} renderItem={} keyExtractor={item => item.id} /> */}
+        {dummyData.map((item) => (
+          <View key={item.id}>
+            <Text>{item.title}</Text>
+            <Text>{item.Duration}</Text>
+          </View>
+        ))}
       </View>
     </SafeAreaView>
   );
