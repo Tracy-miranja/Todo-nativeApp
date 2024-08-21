783b1387b7de20ad2d86772cfe31d57e4b4ee6cc
Tracy-miranja <barazatracy44@gmail.com>
2024-08-21 10:59:06 +0300

    Modify my Todo to add Todos to the existing

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import {
-  StyleSheet,
-  Text,
-  TextInput,
   TouchableOpacity,
+  TextInput,
+  StyleSheet,
   View,
-  SafeAreaView,
-  FlatList,
+  Text,
 } from "react-native";
+import { IconButton } from "react-native-paper";
 
 const dummyData = [
   {
@@ -28,53 +28,70 @@ const dummyData = [
 ];
 
 export default function App() {
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState(dummyData);
+
+  const handleTodo = () => {
+    setTodoList([...todoList, { id: Date.now().toString(), title: todo }]);
+    setTodo("");
+  };
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={{ marginHorizontal: 16 }}>
-        <TextInput
+    <View style={{ marginHorizontal: 16 }}>
+      <TextInput
+        style={{
+          borderWidth: 2,
+          borderColor: "#00ff87",
+          marginTop: 60,
+          borderRadius: 5,
+          paddingVertical: 1,
+          paddingHorizontal: 16,
+        }}
+        value={todo}
+        onChangeText={(userText) => setTodo(userText)}
+        placeholder="Add a task"
+      />
+      <TouchableOpacity
+        style={{
+          backgroundColor: "#000",
+          borderRadius: 20,
+          padding: 2,
+          marginTop: 5,
+          alignItems: "center",
+          marginVertical: 34, // alignText should be alignItems
+        }}
+        onPress={() => handleTodo()}
+      >
+        <Text style={{ color: "#fff", fontWeight: "bold" }}>Add</Text>
+      </TouchableOpacity>
+      {/* render Todo list */}
+      {/* <FlatList data={} renderItem={} keyExtractor={item => item.id} /> */}
+      {todoList.map((item) => (
+        <View
+          key={item.id}
           style={{
-            borderWidth: 2,
-            borderColor: "#00ff87",
-            marginTop: 60,
+            backgroundColor: "#60efff",
             borderRadius: 5,
-            paddingVertical: 1,
-            paddingHorizontal: 16,
-          }}
-          placeholder="Add a task"
-        />
-        <TouchableOpacity
-          style={{
-            backgroundColor: "#000",
-            borderRadius: 20,
-            padding: 2,
+            paddingVertical: 6,
+            paddingHorizontal: 6,
             marginTop: 5,
+            flexDirection: "row",
             alignItems: "center",
-            marginVertical: 34, // alignText should be alignItems
+            justifyContent: "space-around",
           }}
         >
-          <Text style={{ color: "#fff", fontWeight: "bold" }}>Add</Text>
-        </TouchableOpacity>
-        {/* render Todo list */}
-        {/* <FlatList data={} renderItem={} keyExtractor={item => item.id} /> */}
-        {dummyData.map((item) => (
-          <View
-            key={item.id}
-            style={{
-              backgroundColor: "#60efff",
-              borderRadius: 5,
-              paddingVertical: 6,
-              paddingHorizontal: 6,
-              marginTop: 5,
-            }}
+          <Text
+            style={{ color: "#000", fontSize: 14, fontWeight: "800", flex: 1 }}
           >
-            <Text style={{ color: "#000", fontSize: 14, fontWeight: "800" }}>
-              {item.title}
-            </Text>
-            <Text>{item.Duration}</Text>
-          </View>
-        ))}
-      </View>
-    </SafeAreaView>
+            {item.title}
+          </Text>
+          <Text>{item.Duration}</Text>
+          <Text>
+            <IconButton icon="pencil" />
+            <IconButton icon="trash-can" />
+          </Text>
+        </View>
+      ))}
+    </View>
   );
 }
 
